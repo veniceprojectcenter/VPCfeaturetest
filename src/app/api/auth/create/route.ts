0c544445d849b434461454afaec8d238c84b49e1
nick-leslie <46449945+nick-leslie@users.noreply.github.com>
2023-06-14 14:05:50 +0200

    added a create auth route

---
+++ src/app/api/auth/create/route.ts
@@ -0,0 +1,24 @@
+import {NextRequest, NextResponse} from "next/server";
+import {prisma} from "@/app/api/db";
+import { hash } from 'bcrypt';
+interface UserInfo {
+    username: string,
+    password: string
+}
+
+export async function POST(request:NextRequest) {
+    let userInfo:UserInfo = {username:"",password:""};
+    try {
+        userInfo = await request.json();
+    } catch (error) {
+        return NextResponse.json("bad data");
+    }
+    hash(userInfo.password,10,async (err,encrypted) => {
+        let user =  await prisma.maintainer.create({data: {
+                username:userInfo.username,
+                password: encrypted
+            }
+        })
+    })
+    return NextResponse.json("added user")
+}
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import {Navlist} from "@/app/components/nav/Navlist";
-import {useState} from "react";
+import {FormEvent, useState} from "react";
 
 export default function Page() {
     let [searchParam,setSearchParam] = useState("");
@@ -14,7 +14,8 @@ export default function Page() {
             </div>
             <div className={"flex flex-col text-white basis-full"}>
                 <div className={"border-b-2 border-b-white mb-7"}>
-                   <input value={searchParam} onInput={e => {setSearchParam(e.target.value);}} placeholder={"Search Projects..."} className={"bg-transparent w-max h-50 outline-none text-2xl"}/>
+                   <input value={searchParam} onInput={e => { // @ts-ignore
+                       setSearchParam(e.target.value);}} placeholder={"Search Projects..."} className={"bg-transparent w-max h-50 outline-none text-2xl"}/>
                 </div>
                 <div>
                     <Navlist search={searchParam}></Navlist>
