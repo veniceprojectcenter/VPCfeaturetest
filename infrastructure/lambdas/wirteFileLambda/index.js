fb5e98e9a259b4daecfe787ac108178b0240d374
nick-leslie <46449945+nick-leslie@users.noreply.github.com>
2023-07-07 14:59:01 +0200

    lambda

--- infrastructure/lambdas/wirteFileLambda/index.js
+++ infrastructure/lambdas/wirteFileLambda/index.js
@@ -4,13 +4,20 @@ const s3 = new AWS.S3();
 
 //this is the main function body for the serverless lambda function
 exports.handler= async (event,context, callback) => {
-    let params = {
-        Bucket: process.env.bucket, /* required */
-    };
-    //this is just returning all of the buckets as a example
-    s3.listObjectsV2(params, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(data);           // successful response
-    });
-    return "gameing"
+    console.log(JSON.parse(event.body));
+    let body = JSON.parse(event.body);
+    // this gets an upload url
+    const url = s3.getSignedUrl('putObject',{
+        Bucket: process.env.bucket,
+        Key: body.filename,
+        Expires:300 // 5 min timer
+    })
+    const responseBody = {
+        uploadUrl: url,
+        fileKey: body.filename
+    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify(responseBody)
+    }
 }
--- src/app/projects/edit/page.tsx
+++ src/app/projects/edit/page.tsx
@@ -4,9 +4,9 @@ import {Dataurl, IqpTeam, Project} from "@prisma/client";
 import {FullProject} from "@/app/components/ProjectContent/FullProject";
 
 export default function Page() {
-    let project: Project & {iqp_team: IqpTeam | null, dataurls: Dataurl[] | null} = {} as Project & {iqp_team: IqpTeam | null, dataurls: Dataurl[] | null};
+    let project: FullProject = {} as FullProject;
     project.title = "create a project"
-    project.description = "click click on the data elements to edit them"
+    project.description = "click on the data elements to edit them"
     project.year = new Date().getFullYear();
     project.type = "IQP";
     project.dataurls = []
