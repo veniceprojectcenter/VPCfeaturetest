fca313ecea1e1e5816a44810a3e1f4a24f412a60
nick-leslie <46449945+nick-leslie@users.noreply.github.com>
2023-07-10 08:58:24 +0200

    rounded buttons

--- src/app/components/ProjectContent/DataUrl/DataUrlButton.tsx
+++ src/app/components/ProjectContent/DataUrl/DataUrlButton.tsx
@@ -3,7 +3,7 @@ import {Dataurl} from "@prisma/client";
 
 export function DataUrlButton(props:{dataurl:Dataurl}) {
     return(
-        <a href={props.dataurl.url}  key={props.dataurl.id + props.dataurl.type} className={"text-white rounded border-4 text-base"}>
+        <a href={props.dataurl.url}  key={props.dataurl.id + props.dataurl.type} className={"text-white rounded-full border-2 text-base p-2"}>
             {props.dataurl.text}
         </a>
     )
