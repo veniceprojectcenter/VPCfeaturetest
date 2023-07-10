ec95b40de772ac3a72b40fc37fa088e35e482b9d
nick-leslie <46449945+nick-leslie@users.noreply.github.com>
2023-07-10 10:03:26 +0200

    added coments

--- src/helpers/uploadFile.ts
+++ src/helpers/uploadFile.ts
@@ -2,31 +2,34 @@ export async function uploadFile(file:File):Promise<string> {
     console.log(`uploading file ${file.name}`);
     let url = process.env.NEXT_PUBLIC_FILE_API_URL;
     console.log(url)
-    if(url != undefined) {
-        console.log(file.type);
-        const awsResponce = await fetch(url+"/add",{
-            method:"POST",
-            body: JSON.stringify({
-                filename:file.name
-            })
-        })
-        try {
-            let awsBody = await awsResponce.json()
-            let uploadUrl = awsBody.uploadUrl;
-            if(uploadUrl != undefined) {
-                let responce = await fetch(uploadUrl, {
-                    method:"PUT",
-                    headers: {
-                        "Content-Type":"multipart/form-data"
-                    },
-                    body: file
+    if(file.name != undefined) {
+        //TODO upload file on donload results in an undefined file
+        if (url != undefined) {
+            console.log(file.type);
+            const awsResponce = await fetch(url + "/add", {
+                method: "POST",
+                body: JSON.stringify({
+                    filename: file.name
                 })
-                console.log(awsBody.getUrl);
-                return awsBody.getUrl;
+            })
+            try {
+                let awsBody = await awsResponce.json()
+                let uploadUrl = awsBody.uploadUrl;
+                if (uploadUrl != undefined) {
+                    let responce = await fetch(uploadUrl, {
+                        method: "PUT",
+                        headers: {
+                            "Content-Type": "multipart/form-data"
+                        },
+                        body: file
+                    })
+                    console.log(awsBody.getUrl);
+                    return awsBody.getUrl;
+                }
+            } catch (error) {
+                console.log("error");
+                return "failed to upload"
             }
-        } catch (error) {
-            console.log("error");
-            return "failed to upload"
         }
     }
     return "";
