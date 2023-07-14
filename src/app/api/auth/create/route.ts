6d3cc800651b35cebf13639f8e1ac925da618cf7
nick-leslie <46449945+nick-leslie@users.noreply.github.com>
2023-07-14 13:46:23 +0200

    pulled create out of callback

--- src/app/api/auth/create/route.ts
+++ src/app/api/auth/create/route.ts
@@ -10,32 +10,23 @@ export async function POST(request:NextRequest) {
     let userInfo:UserInfo = {username:"",password:""};
     try {
         userInfo = await request.json();
-        console.log("got user info" + userInfo)
+        console.log("got user info")
     } catch (error) {
         return NextResponse.json("bad data");
     }
     console.log("past user info awaiting hash")
-    await hash(userInfo.password,10,async (err,encrypted) => {
-        console.log("in hash")
-        if(err != undefined) {
-            return NextResponse.json(err)
+    console.log(userInfo)
+    let hashedPassword = await hash(userInfo.password,10);
+    let user = await prisma.maintainer.create({
+        data: {
+            username: userInfo.username,
+            password: hashedPassword
         }
-        try {
-            console.log("trying to add hash")
-            let user = await prisma.maintainer.create({
-                data: {
-                    username: userInfo.username,
-                    password: encrypted
-                }
-            })
-            return NextResponse.json({
-                text: "created user",
-                user: user
-            })
-        } catch (err) {
-            return NextResponse.json(err);
-        }
-
     })
-    return NextResponse.json("added user")
+    console.log("created user")
+    console.log(user)
+    return NextResponse.json({
+        text: "created user",
+        user: user
+    })
 }
