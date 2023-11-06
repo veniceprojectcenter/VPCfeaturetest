1cc3acda27245950d65102fb8f96c58af3039fba
Alireza Dehghan <alirezaxdehghan@gmail.com>
2023-11-06 11:44:42 +0100

    fixed some line breaks

--- src/app/components/ProjectContent/editingCode/TagInputBox.tsx
+++ src/app/components/ProjectContent/editingCode/TagInputBox.tsx
@@ -3,15 +3,16 @@ import {ProjectWidget} from "@/app/components/nav/projectWidget";
 
 export function TagInputBox(props: {unprocessedTags: string[]}) {
     return (
-        <>
-            {props.unprocessedTags.map((tag: string) => {
+        props.unprocessedTags.map((tag: string) => {
                     return (
+                        <>
                         <label key={tag}>
                             {tag}
                             <input type={"checkbox"} value={tag}/>
-                        </label>
+                        </label><br/>
+                        </>
                     )
                 }
-            )}
-        </>);
+            )
+    );
 }
