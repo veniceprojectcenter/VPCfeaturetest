87354753ccc83b224f312f935f221f30d2451e5c
venice-project-center <139366559+venice-project-center@users.noreply.github.com>
2023-11-15 10:49:16 +0100

    Update Footer.tsx to fix the address

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -12,7 +12,7 @@ export function Footer() {
                 <p className = {""}>
                     <Link href={"https://www.google.com/maps/place/H3+(Ex+Herion)+%E2%80%93+Ex+Chiesa+Santi+Cosma+e+Damiano/@45.4257633,12.320547,17z/data=!3m1!4b1!4m6!3m5!1s0x477eb03320023f75:0x6fa0b96420b7a8fc!8m2!3d45.4257633!4d12.3231219!16s%2Fg%2F11c52pv__5?entry=ttu"} target={"_blank"}>
                         H3 (Ex-Herion)<br className={""}/>
-                        Campo SS. Cosma e Damiano, 624-625<br className={""}/>
+                        Campo SS. Cosma e Damiano, 624<br className={""}/>
                         Giudecca
                         30133 – Venice - Italy
                     </Link>
